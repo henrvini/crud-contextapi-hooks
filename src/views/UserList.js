90de7d14bf70bfce457363c6220cfb6e1524aaf2
Vinicius Rodrigues <vini0593@gmail.com>
2022-09-02 16:18:58 -0300

    added button edit and delete

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,13 +1,44 @@
-import { FlatList, View } from "react-native";
-import { Avatar, ListItem } from "react-native-elements";
+import { Alert, FlatList, View } from "react-native";
+import { Avatar, Button, Icon, ListItem } from "react-native-elements";
 
 import users from "../data/users";
 
 const UserList = ({ navigation }) => {
+  function confirmUserDeletion(user) {
+    Alert.alert("Excluir usuário", "Deseja excluir usuário permanentemente?", [
+      {
+        text: "Sim",
+        onPress() {
+          console.warn(`Deleted user ${user.name}`);
+        },
+      },
+      {
+        text: "Não",
+      },
+    ]);
+  }
+
+  function getActions(user) {
+    return (
+      <>
+        <Button
+          onPress={() => navigation.navigate("UserForm", user)}
+          type="clear"
+          icon={<Icon name="edit" size={25} color={"black"} />}
+        />
+        <Button
+          onPress={() => confirmUserDeletion(user)}
+          type="clear"
+          icon={<Icon name="delete" size={25} color={"red"} />}
+        />
+      </>
+    );
+  }
+
   function getUserItem({ item: user }) {
     return (
       <ListItem
-        onPress={() => navigation.navigate("UserForm")}
+        onPress={() => navigation.navigate("UserForm", user)}
         key={user}
         bottomDivider
       >
@@ -16,9 +47,11 @@ const UserList = ({ navigation }) => {
           <ListItem.Title>{user.name}</ListItem.Title>
           <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
         </ListItem.Content>
+        {getActions(user)}
       </ListItem>
     );
   }
+
   return (
     <FlatList
       data={users}
