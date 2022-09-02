aed2ebfbfdeefd813a019fd296ec1e007c463958
Vinicius Rodrigues <vini0593@gmail.com>
2022-09-02 15:59:43 -0300

    added users list

--- src/views/UserList.js
+++ src/views/UserList.js
@@ -1,10 +1,30 @@
-import { TabView, Text } from "react-native-elements";
+import { FlatList, View } from "react-native";
+import { Avatar, ListItem } from "react-native-elements";
 
-const UserList = () => {
+import users from "../data/users";
+
+const UserList = ({ navigation }) => {
+  function getUserItem({ item: user }) {
+    return (
+      <ListItem
+        onPress={() => navigation.navigate("UserForm")}
+        key={user}
+        bottomDivider
+      >
+        <Avatar source={{ uri: user.avatarUrl }} />
+        <ListItem.Content>
+          <ListItem.Title>{user.name}</ListItem.Title>
+          <ListItem.Subtitle>{user.email}</ListItem.Subtitle>
+        </ListItem.Content>
+      </ListItem>
+    );
+  }
   return (
-    <TabView>
-      <Text>User List</Text>
-    </TabView>
+    <FlatList
+      data={users}
+      keyExtractor={(user) => user.id.toString()}
+      renderItem={getUserItem}
+    ></FlatList>
   );
 };
 
