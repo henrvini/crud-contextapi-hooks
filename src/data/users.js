46bedc0aec847b5c62f8ca0f97271a4210e8062b
Vinicius Rodrigues <vini0593@gmail.com>
2022-09-02 13:46:36 -0300

    added users

---
+++ src/data/users.js
@@ -0,0 +1,30 @@
+export default [
+  {
+    id: 1,
+    name: "João Silva",
+    email: "joao.silva@email.com",
+    avatarUrl:
+      "https://cdn.pixabay.com/photo/2016/03/31/19/58/avatar-1295429_960_720.png",
+  },
+  {
+    id: 2,
+    name: "Mariana Aguiar",
+    email: "mariana.aguiar@email.com",
+    avatarUrl:
+      "https://cdn.pixabay.com/photo/2016/08/20/05/36/avatar-1606914_960_720.png",
+  },
+  {
+    id: 3,
+    name: "Julio Fragoso",
+    email: "julio.fragoso@email.com",
+    avatarUrl:
+      "https://cdn.pixabay.com/photo/2014/04/03/10/32/businessman-310819_960_720.png",
+  },
+  {
+    id: 4,
+    name: "Rafael Monteiro",
+    email: "rafael.monteiro@email.com",
+    avatarUrl:
+      "https://cdn.pixabay.com/photo/2016/08/20/05/38/avatar-1606916_960_720.png",
+  },
+];
